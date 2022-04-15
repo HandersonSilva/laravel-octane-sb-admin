d3eb20fa33583e719ed8d98edfff6014fa52fae0
Handerson Silva <handerson.silva@dotlib.com.br>
2022-04-15 17:08:30 -0300

    [feat] Adicionado configuração do spatie/laravel-permission

--- docker/php/docker-start.sh
+++ docker/php/docker-start.sh
@@ -6,4 +6,7 @@ php artisan config:clear
 php artisan route:clear
 php artisan optimize:clear
 
+php artisan migrate --force
+php artisan db:seed --force
+
 php artisan octane:start --watch --host 0.0.0.0 --port 9000
